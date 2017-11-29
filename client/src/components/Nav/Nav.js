4646ad38661adf009643108a6997f523dcc27d56
joe-fuentes <joevfuentes@aol.com>
2017-11-28 19:34:50 -0500

    Changes to Nav.js

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,9 +5,32 @@ const Nav = () => (
   <nav className="navbar navbar-inverse navbar-top">
     <div className="container-fluid">
       <div className="navbar-header">
-        <a href="/" className="navbar-brand">
-          Business in a Box
-        </a>
+        <a href="/" className="navbar-brand">Home</a>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="/Buy" className="navbar-brand">Buy</a>
+                </li>
+                <li>
+                    <a href="/Accounts" className="navbar-brand">Accounts</a>
+                </li>
+                <li>
+                    <a href="/Production" className="navbar-brand">Production</a>
+                </li>
+                <li>
+                    <a href="/Sell" className="navbar-brand">Sell</a>
+                </li>
+                <li>
+                    <a href="/Support" className="navbar-brand">Support</a>
+                </li>
+                <li>
+                    <a href="/Setting" className="navbar-brand">Setting</a>
+                </li>
+                <li>
+                    <a href="/AboutUs" className="navbar-brand">About Us</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
       </div>
     </div>
   </nav>
