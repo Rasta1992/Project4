3355012589abdbc277df2df7592eddf59ea60944
joe-fuentes <joevfuentes@aol.com>
2017-12-01 18:59:07 -0500

    Added Dropdown component

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
   <div className="jumbotron text-center">
-  <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Hexahedron.gif" alt="Business Cube"/>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Hexahedron.gif" alt="Business Cube" width="114" height="114"/>
     <h1>Business in a Box</h1>
     <a
       target="_blank"
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -13,16 +13,27 @@ const Nav = () => (
       </button>
       <a className="navbar-brand brand" href="/">BusinessBox</a>
     </div>
-        <a href="/" className="navbar-brand">Home</a>
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-                <li><a href="/Purchase" className="navbar-brand">Purchase</a></li>
-                <li><a href="/Production" className="navbar-brand">Production</a></li>
-                <li><a href="/Sold" className="navbar-brand">Sold!</a></li>
-                <li><a href="/Support" className="navbar-brand">Support</a></li>
-                <li><a href="/Settings" className="navbar-brand">Settings</a></li>
-                <li><a href="/AboutUs" className="navbar-brand">About Us</a></li>
-            </ul>
+
+    <a href="/" className="navbar-brand">Home</a>
+
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul className="nav navbar-nav navbar-right">
+            <li><a href="/Purchase" className="navbar-brand">Purchase</a></li>
+            <li><a href="/Production" className="navbar-brand">Production</a></li>
+            <li><a href="/Sold" className="navbar-brand">Sold!</a></li>
+            <li className="dropdown">
+                <a href="#" className="navbar-brand" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Support<span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                    <li><a href="/Management" className="navbar-brand">Management</a></li>
+                    <li><a href="/HR" className="navbar-brand">HR</a></li>
+                    <li><a href="/Accounting" className="navbar-brand">Accounting</a></li>
+                    <li><a href="/Maintenance" className="navbar-brand">Maintenance</a></li>
+                    <li><a href="/Shipping" className="navbar-brand">Shipping</a></li>
+                </ul>
+            </li>
+            <li><a href="/Settings" className="navbar-brand">Settings</a></li>
+            <li><a href="/AboutUs" className="navbar-brand">About Us</a></li>
+        </ul>
       </div>
     </div>
   </nav>
