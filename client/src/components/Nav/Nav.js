4077fcf7d6da38da258619e13320f4374d6e7c27
joe-fuentes <joevfuentes@aol.com>
2017-12-01 17:36:02 -0500

    Modified Navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,17 +4,28 @@ import "./Nav.css";
 const Nav = () => (
   <nav className="navbar navbar-inverse navbar-top">
     <div className="container-fluid">
-      <div className="navbar-header">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand brand" href="/">BusinessBox</a>
+    </div>
         <a href="/" className="navbar-brand">Home</a>
-        <a href="/Purchasing" className="navbar-brand">Purchasing</a>
-        <a href="/Production" className="navbar-brand">Production</a>
-        <a href="/Sold" className="navbar-brand">Sold!</a>
-        <a href="/Support" className="navbar-brand">Support</a>
-        <a href="/Setting" className="navbar-brand">Setting</a>
-        <a href="/AboutUs" className="navbar-brand">About Us</a>
-      </div><!-- /.navbar-header -->
-    </div><!-- /.container-fluid -->
-  </nav><!-- navbar navbar-inverse navbar-top -->
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav navbar-right">
+                <li><a href="/Purchase" className="navbar-brand">Purchase</a></li>
+                <li><a href="/Production" className="navbar-brand">Production</a></li>
+                <li><a href="/Sold" className="navbar-brand">Sold!</a></li>
+                <li><a href="/Support" className="navbar-brand">Support</a></li>
+                <li><a href="/Settings" className="navbar-brand">Settings</a></li>
+                <li><a href="/AboutUs" className="navbar-brand">About Us</a></li>
+            </ul>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Nav;
