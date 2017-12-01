e54726a74379c43ccfbc7107b7c6e0bb2066db95
joe-fuentes <joevfuentes@aol.com>
2017-12-01 16:42:22 -0500

    Modified the Navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,34 +6,15 @@ const Nav = () => (
     <div className="container-fluid">
       <div className="navbar-header">
         <a href="/" className="navbar-brand">Home</a>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="/Buy" className="navbar-brand">Buy</a>
-                </li>
-                <li>
-                    <a href="/Accounts" className="navbar-brand">Accounts</a>
-                </li>
-                <li>
-                    <a href="/Production" className="navbar-brand">Production</a>
-                </li>
-                <li>
-                    <a href="/Sell" className="navbar-brand">Sell</a>
-                </li>
-                <li>
-                    <a href="/Support" className="navbar-brand">Support</a>
-                </li>
-                <li>
-                    <a href="/Setting" className="navbar-brand">Setting</a>
-                </li>
-                <li>
-                    <a href="/AboutUs" className="navbar-brand">About Us</a>
-                </li>
-            </ul>
-        </div> 
-      </div>
-    </div>
-  </nav>
+        <a href="/Purchasing" className="navbar-brand">Purchasing</a>
+        <a href="/Production" className="navbar-brand">Production</a>
+        <a href="/Sold" className="navbar-brand">Sold!</a>
+        <a href="/Support" className="navbar-brand">Support</a>
+        <a href="/Setting" className="navbar-brand">Setting</a>
+        <a href="/AboutUs" className="navbar-brand">About Us</a>
+      </div><!-- /.navbar-header -->
+    </div><!-- /.container-fluid -->
+  </nav><!-- navbar navbar-inverse navbar-top -->
 );
 
 export default Nav;
