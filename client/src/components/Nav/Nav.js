5085809a280b786398af93f3fb280bc0bd0cb2ef
joe-fuentes <joevfuentes@aol.com>
2017-12-02 13:29:15 -0500

    Added <Router></Router> around the Nav.js code

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,38 +2,39 @@ import React from "react";
 import "./Nav.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-const Nav = () => (
+const Nav = () => (<Router>
   <nav className="navbar navbar-inverse navbar-top">
     <div className="container-fluid">
-    <div className="navbar-header">
-      <a className="navbar-brand brand" href="/">BusinessBox</a>
-      <a href="/" className="navbar-brand">Home</a>
+
+          <div className="navbar-header">
+      <Link to="/" className="navbar-brand brand">BusinessBox</Link>
+      <Link to="/" className="navbar-brand">Home</Link>
     </div>
 
-<Router>
+
     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul className="nav navbar-nav navbar-right">
             <li><Link to="/Purchase" className="navbar-brand">Purchase</Link></li>
-            <li><a href="/Production" className="navbar-brand">Production</a></li>
-            <li><a href="/Sold" className="navbar-brand">Sold!</a></li>
+            <li><Link to="/Production" className="navbar-brand">Production</Link></li>
+            <li><Link to="/Sold" className="navbar-brand">Sold!</Link></li>
             <li className="dropdown">
-                <a href="#" className="navbar-brand" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Support<span className="caret"></span></a>
+                <Link to="#" className="navbar-brand" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Support<span className="caret"></span></Link>
                 <ul className="dropdown-menu">
-                    <li><a href="/Management" className="navbar-brand">Management</a></li>
-                    <li><a href="/HR" className="navbar-brand">HR</a></li>
-                    <li><a href="/Accounting" className="navbar-brand">Accounting</a></li>
-                    <li><a href="/Maintenance" className="navbar-brand">Maintenance</a></li>
-                    <li><a href="/Shipping" className="navbar-brand">Shipping</a></li>
+                    <li><Link to="/Management" className="navbar-brand">Management</Link></li>
+                    <li><Link to="/HR" className="navbar-brand">HR</Link></li>
+                    <li><Link to="/Linkccounting" className="navbar-brand">Accounting</Link></li>
+                    <li><Link to="/Maintenance" className="navbar-brand">Maintenance</Link></li>
+                    <li><Link to="/Shipping" className="navbar-brand">Shipping</Link></li>
                 </ul>
             </li>
-            <li><a href="/Settings" className="navbar-brand">Settings</a></li>
-            <li><a href="/AboutUs" className="navbar-brand">About Us</a></li>
+            <li><Link to="/Settings" className="navbar-brand">Settings</Link></li>
+            <li><Link to="/LinkboutUs" className="navbar-brand">About Us</Link></li>
         </ul>
       </div>
-      </Router>
     </div>
    
   </nav>
+</Router>
 );
 
 export default Nav;
